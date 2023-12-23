2af9b1922dba8bc52b1b9de839f6b7a0d163423c
Jason Sun <sunapi386@gmail.com>
2023-12-23 10:14:32 -0500

    Refactor fetch_posts function and add dotenv support

--- backend/fetch_posts.py
+++ backend/fetch_posts.py
@@ -1,17 +1,30 @@
 # PRAW (Python Reddit API Wrapper) library to fetch posts `pip install praw`
 # Reddit's developer portal to get your client_id, client_secret, and user_agent.
 
+import os
+import time
 import praw
+from dotenv import load_dotenv
 
-def fetch_posts(subreddit, client_id, client_secret, user_agent):
-    reddit = praw.Reddit(client_id=client_id, 
-                         client_secret=client_secret, 
-                         user_agent=user_agent)
+load_dotenv()
+
+
+def fetch_posts(subreddit):
+    client_id = os.getenv("REDDIT_API_ID")
+    client_secret = os.getenv("REDDIT_API_SECRET")
+    user_agent = "script:wallstreetbets_top:v1.0"
+
+    reddit = praw.Reddit(
+        client_id=client_id, client_secret=client_secret, user_agent=user_agent
+    )
 
     posts = []
-    for post in reddit.subreddit(subreddit).new(limit=1000):  # Adjust the limit as needed
+    for post in reddit.subreddit(subreddit).new(limit=10):  # Adjust the limit as needed
         if post.created_utc > time.time() - 86400:  # Last 24 hours
-            posts.append(post.title + ' ' + post.selftext)  # Combining title and body
+            posts.append(post.title + " " + post.selftext)  # Combining title and body
 
     return posts
 
+
+if __name__ == "__main__":
+    print(fetch_posts("wallstreetbets"))
