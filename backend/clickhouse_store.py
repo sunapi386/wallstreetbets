9dab5ed40d18aa90aa7caf8d6df2910db43b9adb
Jason Sun <sunapi386@gmail.com>
2024-01-02 23:13:35 -0500

    Update ClickHouse table schema and add environment variable checks

--- backend/clickhouse_store.py
+++ backend/clickhouse_store.py
@@ -2,32 +2,46 @@
 import datetime
 import os
 from clickhouse_driver import Client  # pip install clickhouse-driver
-
 from dotenv import load_dotenv
 
-load_dotenv()
-
-hostname = os.getenv("CLICKHOUSE_HOST") or "localhost"
-username = os.getenv("CLICKHOUSE_USER") or "default"
-password = os.getenv("CLICKHOUSE_PASSWORD") or ""
-database = os.getenv("CLICKHOUSE_DATABASE") or "default"
-
-# Connect to ClickHouse
-client = Client(host=hostname, user=username, password=password, database=database)
-
-# Assuming a table schema like this (modify according to your actual table schema):
+"""
+CREATE USER u IDENTIFIED WITH plaintext_password BY 'pass';
+CREATE DATABASE db;
+GRANT ALL ON db.* TO u;
+"""
+"""
+DROP TABLE IF EXISTS reddit_posts
+"""
 """
 CREATE TABLE reddit_posts (
     title String,
     content String,
     summary String,
     sentiment Float64,
-    stocks Array(String),
+    entities Array(String),
     created_utc DateTime,
-    url String
-) ENGINE = MergeTree()
-ORDER BY created_utc;
+    url String,
+    last_updated DateTime DEFAULT now()
+) ENGINE = ReplacingMergeTree(last_updated)
+ORDER BY (title, content, url, created_utc);
+
 """
+
+
+load_dotenv()
+
+hostname = os.getenv("CLICKHOUSE_HOST")
+username = os.getenv("CLICKHOUSE_USER")
+password = os.getenv("CLICKHOUSE_PASSWORD")
+database = os.getenv("CLICKHOUSE_DATABASE")
+
+assert hostname, "CLICKHOUSE_HOST environment variable not set"
+assert username, "CLICKHOUSE_USER environment variable not set"
+assert password, "CLICKHOUSE_PASSWORD environment variable not set"
+assert database, "CLICKHOUSE_DATABASE environment variable not set"
+
+# Connect to ClickHouse
+client = Client(host=hostname, user=username, password=password, database=database)
 
 
 def insert_posts_into_clickhouse(posts):
@@ -38,8 +52,8 @@ def insert_posts_into_clickhouse(posts):
             post["content"],
             post["summary"],
             post["sentiment"],
-            post["stocks"],
-            datetime.fromtimestamp(post["created_utc"]),
+            post["entities"],
+            post["created_utc"],
             post["url"],
         )
         for post in posts
@@ -47,14 +61,14 @@ def insert_posts_into_clickhouse(posts):
 
     # Insert the data
     client.execute(
-        "INSERT INTO reddit_posts (title, content, summary, sentiment, stocks, created_utc, url) VALUES",
+        "INSERT INTO reddit_posts (title, content, summary, sentiment, entities, created_utc, url) VALUES",
         data_to_insert,
     )
 
 
-def get_most_recent_date():
-    result = client.execute("SELECT max(created_utc) FROM reddit_posts")
-    return result[0][0]
+def get_processed_posts():
+    result = client.execute("SELECT created_utc, title, last_updated FROM reddit_posts")
+    return result
 
 
 if __name__ == "__main__":
@@ -64,8 +78,9 @@ if __name__ == "__main__":
         "content": "Netflix is going down, and I'm going down with it.",
         "summary": "Netflix is going down, and I'm going down with it.",
         "sentiment": 0.0,
-        "stocks": ["NFLX"],
+        "entities": ["NFLX"],
         "created_utc": 1360684800,
         "url": "https://www.reddit.com/r/wallstreetbets/comments/18ls2i6/netflix_is_going_down/",
     }
     insert_posts_into_clickhouse([example_post])
+    print(get_processed_posts())
